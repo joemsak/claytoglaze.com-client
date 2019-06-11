4427654eba2a12db3a021fab774c357460b38750
Joe Sak <joe@joesak.com>
2019-06-10 19:45:22 -0500

    Port c2gclay.com Vue to this React app

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,136 @@
 import React from "react";
+import $ from 'jquery';
 import { graphqlOperation } from "aws-amplify";
 import { Connect } from "aws-amplify-react";
 import * as queries from "./graphql/queries";
 import { ListAlbumsQuery } from "./API";
 
-const Album = () => {
-  interface ListAlbumsQueryProps {
-    albums: ListAlbumsQuery;
+interface ListAlbumsQueryProps {
+  albums: ListAlbumsQuery;
+}
+
+const AlbumList = (albumsProps: ListAlbumsQueryProps) => {
+  const { albums } = albumsProps;
+
+  const renderPhoto = ({ urls, id } : any) => {
+    return <img
+      key={id}
+      alt="Forgive us, the description is pending..."
+      width="100%"
+      srcSet={`
+        ${urls.RES_2} 640w,
+        ${urls.RES_3} 1125w,
+        ${urls.RES_1} 2000w
+      `}
+      sizes="100vw"
+      src={urls.RES_1}
+    />
   }
 
-  const AlbumList = (albumsProps: ListAlbumsQueryProps) => {
-    const { albums } = albumsProps;
+  const renderPhotos = (albumId : string) => {
+    return <Connect query={graphqlOperation(queries.getAlbum, { id: albumId })}>
+      {({data, errors, loading}:any) => {
+        if (loading) return null;
+        if (errors.length) return null;
+        if (!data.getAlbum) return null;
 
-    const renderPhoto = (url : string) => {
-      return <li key={url}>
-        <a href={url} target="_blank" rel="noopener noreferrer">
-          { url }
-        </a>
-      </li>;
+        return data.getAlbum.photos.items.map(renderPhoto);
+      }}
+    </Connect>;
+  }
+
+  const renderAlbum = ({ id, name }:any) => {
+    return renderPhotos(id)
+  }
+
+  const renderAlbums = (albums : any) => {
+    if (albums.length) {
+      return albums.map(renderAlbum);
+    } else {
+      return 'No albums...';
     }
+  }
 
-    const renderPhotos = (albumId : string) => {
-      return <Connect query={graphqlOperation(queries.getAlbum, { id: albumId })}>
-        {({data, errors, loading}:any) => {
-          if (loading)
-            return <p>Loading photos...</p>;
+  const scrollToGallery = () => {
+    const target = $("#gallery")
 
-          if (errors.length)
-            return <p>{`${JSON.stringify(errors)}`}</p>;
+    $('html, body').animate({
+      scrollTop: target.offset()!.top
+    }, 500, function() {
+      var $target = $(target)
+      $target.focus()
 
-          if (!data.getAlbum)
-            return null;
-
-          return <ul>
-            {data.getAlbum.photos.items.flatMap(({ urls }:any) => Object.values(urls)).map(renderPhoto)}
-          </ul>;
-        }}
-      </Connect>;
-    }
-
-    const renderAlbum = ({ id, name }:any) => {
-      return <li key={id}>
-        { name }
-        { renderPhotos(id) }
-      </li>;
-    }
-
-    const renderAlbums = (albums : any) => {
-      if (albums.length) {
-        return <ul>
-          { albums.map(renderAlbum) }
-        </ul>;
+      if ($target.is(":focus")) {
+        return false
       } else {
-        return 'No albums...';
+        $target.attr('tabindex','-1')
+        $target.focus()
       }
-    }
-
-    return (
-      <div>
-        <h3>Albums</h3>
-        {albums && albums.listAlbums && albums.listAlbums.items
-          ? renderAlbums(albums.listAlbums.items)
-          : null}
-      </div>
-    );
+    })
   };
 
-  return (
-    <Connect query={graphqlOperation(queries.listAlbums)}>
-      {({
-        data,
-        loading,
-        error
-      }: {
-        data: ListAlbumsQuery;
-        loading: boolean;
-        error: Array<Error>;
-      }) => {
-        if (error) return <h3>Error</h3>;
-        if (loading || !data) return <h3>Loading...</h3>;
-        return <AlbumList albums={data} />;
-      }}
-    </Connect>
-  );
+  if (!albums.listAlbums) return null;
+
+  return <div id="app" className="container-fluid">
+    <div id="logo-wrapper">
+      <div id="logo" className="mt-5"></div>
+    </div>
+
+    <div id="cover" className="d-flex justify-content-center">
+      <i onClick={scrollToGallery} className="fas fa-chevron-down"></i>
+    </div>
+
+    <div id="gallery">
+      { renderAlbums(albums.listAlbums.items) }
+    </div>
+  </div>;
+
 };
 
-export default Album;
+class App extends React.Component {
+  componentDidMount () {
+    window.onscroll = function() { scrollFunction() }
+
+    function scrollFunction() {
+      const logo = (document.querySelector("#logo") as HTMLElement)!
+
+      const coverHeight = (
+        document.querySelector("#cover") as HTMLElement
+      )!.offsetHeight * 0.55
+
+      if (document.body.scrollTop > coverHeight || document.documentElement.scrollTop > coverHeight) {
+        logo.style.width = "50px"
+        logo.style.height = "50px"
+        logo.classList.remove("mt-5")
+        logo.classList.add("ml-3", "mt-3")
+      } else {
+        logo.style.width = "250px"
+        logo.style.height = "250px"
+        logo.classList.add("mt-5")
+        logo.classList.remove("ml-3", "mt-3")
+      }
+    }
+  }
+
+  render () {
+    return (
+      <Connect query={graphqlOperation(queries.listAlbums)}>
+        {({
+          data,
+          loading,
+          error
+        }: {
+          data: ListAlbumsQuery;
+          loading: boolean;
+          error: Array<Error>;
+        }) => {
+          if (error) return null;
+          if (loading || !data) return null;
+          return <AlbumList albums={data} />;
+        }}
+      </Connect>
+    );
+  }
+};
+
+export default App;
